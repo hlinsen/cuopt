0cf9f5fa74c0ba00650b7a3f251e68a932c52723
Nicolas Blin <nicolas.blin7@hotmail.fr>
2025-08-28 15:13:32 +0200

    add presolve option to run pdlp

--- benchmarks/linear_programming/cuopt/run_pdlp.cu
+++ benchmarks/linear_programming/cuopt/run_pdlp.cu
@@ -78,6 +78,12 @@ static void parse_arguments(argparse::ArgumentParser& program)
       "Path to PDLP hyper-params file to configure PDLP solver. Has priority over PDLP solver "
       "modes.");
 
+  program.add_argument("--presolve")
+    .help("enable/disable presolve (default: true for MIP problems, false for LP problems)")
+    .default_value(0)
+    .scan<'i', int>()
+    .choices(0, 1);
+
   program.add_argument("--solution-path").help("Path where solution file will be generated");
 }
 
@@ -106,6 +112,7 @@ static cuopt::linear_programming::pdlp_solver_settings_t<int, double> create_sol
     string_to_pdlp_solver_mode(program.get<std::string>("--pdlp-solver-mode"));
   settings.method = static_cast<cuopt::linear_programming::method_t>(program.get<int>("--method"));
   settings.crossover = program.get<int>("--crossover");
+  settings.presolve  = program.get<int>("--presolve");
 
   return settings;
 }
