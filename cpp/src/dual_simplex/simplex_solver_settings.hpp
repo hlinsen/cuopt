194193b945946057705b752b18a448ffee856cbf
Hugo Linsenmaier <hlinsenmaier@gmail.com>
2025-11-18 15:33:08 -0800

    Address review comments

--- cpp/src/dual_simplex/simplex_solver_settings.hpp
+++ cpp/src/dual_simplex/simplex_solver_settings.hpp
@@ -74,8 +74,7 @@ struct simplex_solver_settings_t {
       inside_mip(0),
       solution_callback(nullptr),
       heuristic_preemption_callback(nullptr),
-      concurrent_halt(nullptr),
-      num_gpus(1)
+      concurrent_halt(nullptr)
   {
   }
 
